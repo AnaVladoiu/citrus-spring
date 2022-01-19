5ad312f3cebb07ad9fdc947d42fc6969a29cf30a
Ana <qjk202f@gmail.com>
2022-01-19 16:29:19 +0200

    initial commit

---
+++ src/test/java/com/example/citrus/citrusspring/CitrusSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citrus.citrusspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitrusSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/citrus/citrusspring/TestCitrusIT.java
@@ -0,0 +1,22 @@
+package com.example.citrus.citrusspring;
+
+import org.testng.annotations.Test;
+
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+
+@Test
+public class TestCitrusIT extends TestNGCitrusTestDesigner{
+
+    @CitrusTest
+    public void echoToday() {
+        variable("now", "citrus:currentDate()");
+
+        echo("Today is: ${now}");
+    }
+
+    @CitrusTest(name = "SampleJavaTest.sayHello")
+    public void sayHello() {
+        echo("Hello Citrus!");
+    }
+}
